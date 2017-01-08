217086208b75366e7919d3094277f380b4042c80
oguzhanoya <oguzhanoya@outlook.com>
2017-01-08 04:56:18 +0200

    Changed environment

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,5 @@
-// production environment
-process.env.NODE_ENV = 'production';
+// test environment
+process.env.NODE_ENV = 'test';
 
 const app = require('../app');
 const request = require('supertest')(app);
